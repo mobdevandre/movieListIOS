b11b5e91b812b1dc938959a8baca9ca10c6530da
Andre Lemos <166083332+mobdevandre@users.noreply.github.com>
2024-05-14 13:21:38 -0300

    Initial Commit

---
+++ MovieList/MovieListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListApp.swift
+//  MovieList
+//
+//  Created by Andre Lemos on 2024-05-14.
+//
+
+import SwiftUI
+
+@main
+struct MovieListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
