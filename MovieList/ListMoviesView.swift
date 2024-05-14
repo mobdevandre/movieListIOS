a121166bd82ccfa9a89ef5239f47ea03476dfc02
Andre Lemos <166083332+mobdevandre@users.noreply.github.com>
2024-05-14 13:44:45 -0300

    first commit

---
+++ MovieList/ListMoviesView.swift
@@ -0,0 +1,18 @@
+//
+//  ListMoviesView.swift
+//  MovieList
+//
+//  Created by Andre Lemos on 2024-05-14.
+//
+
+import SwiftUI
+
+struct ListMoviesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListMoviesView()
+}
